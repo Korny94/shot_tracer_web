8f746870d66d56ef1aba4465d7c9f8ff669e5449
Karl Magnus Leonardo Nøkling <114525120+Korny94@users.noreply.github.com>
2025-12-22 10:10:47 +0100

    update

--- src/App.tsx
+++ src/App.tsx
@@ -1,36 +1,3 @@
-// import { Routes, Route } from "react-router-dom";
-// import ScrollToTop from "./components/components";
-// import Home from "./pages/Home";
-// import ShotTracer from "./pages/ShotTracer";
-// import AboutPage from "./pages/AboutPage";
-// import TermsPage from "./pages/TermsPage";
-// import ContactPage from "./pages/ContactPage";
-// import FAQPage from "./pages/FAQPage";
-// import Footer from "./components/Footer";
-// import Navbar from "./components/Navbar";
-// import "./index.css";
-
-// const App = () => (
-//   <div className="flex flex-col min-h-screen bg-black">
-//     <Navbar />
-//     <main className="flex-grow">
-//       <Routes>
-//         <Route path="/" element={<Home />} />
-//         <Route path="/shot-tracer" element={<ShotTracer />} />
-//         <Route path="/about" element={<AboutPage />} />
-//         <Route path="/contact" element={<ContactPage />} />
-//         <Route path="/faq" element={<FAQPage />} />
-//         <Route path="/terms" element={<TermsPage />} />
-//         <Route path="/privacy" element={<TermsPage />} />
-//       </Routes>
-//     </main>
-//     <Footer />
-//     <ScrollToTop />
-//   </div>
-// );
-
-// export default App;
-
 import { Routes, Route, useLocation } from "react-router-dom";
 import ScrollToTop from "./components/components";
 import Home from "./pages/Home";
